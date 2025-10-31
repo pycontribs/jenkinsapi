27cfe7740102e8db0d2ec0c789e80ab4a87cb054
CJ Steiner <clintonsteiner@gmail.com>
2025-10-31 17:50:15 -0500

    reenable: test_env_vars test - was failing due to plugin incompatibility

--- jenkinsapi_tests/systests/test_env_vars.py
+++ jenkinsapi_tests/systests/test_env_vars.py
@@ -3,13 +3,11 @@ System tests for `jenkinsapi.jenkins` module.
 """
 
 import time
-import pytest
 
 from jenkinsapi_tests.systests.job_configs import JOB_WITH_ENV_VARS
 from jenkinsapi_tests.test_utils.random_strings import random_string
 
 
-@pytest.mark.skip(reason="Intermittently failing test")
 def test_get_env_vars(jenkins):
     job_name = "get_env_vars_create1_%s" % random_string()
     job = jenkins.create_job(job_name, JOB_WITH_ENV_VARS)
