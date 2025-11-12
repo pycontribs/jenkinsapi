5c08e41735e0ef8d89d0c3ad59190563cee0a830
CJ Steiner <clintonsteiner@gmail.com>
2025-11-12 06:47:03 -0600

    test_scm: block til complete

--- jenkinsapi_tests/systests/test_scm.py
+++ jenkinsapi_tests/systests/test_scm.py
@@ -8,6 +8,8 @@ from jenkinsapi_tests.test_utils.random_strings import random_string
 from jenkinsapi_tests.systests.job_configs import (
     SCM_GIT_JOB,
     MULTIBRANCH_GIT_BRANCH_JOB_PROPERTY,
+    MULTIBRANCH_GIT_SCM_JOB,
+    MULTIBRANCH_GITHUB_SCM_JOB,
 )
 
 
@@ -23,6 +25,9 @@ def wait_for_job_setup(jenkins, job_name):
 def test_get_scm_type(jenkins):
     job_name = "git_%s" % random_string()
     job = jenkins.create_job(job_name, SCM_GIT_JOB)
+    queueItem = job.invoke()
+    queueItem.block_until_complete()
+
     wait_for_job_setup(jenkins, job_name)
     compare(job.get_scm_type(), "git")
     jenkins.delete_job(job_name)
@@ -33,30 +38,36 @@ def test_get_scm_type_pipeline_scm_multibranch_BranchJobProperty(
 ):
     job_name = "git_%s" % random_string()
     job = jenkins.create_job(job_name, MULTIBRANCH_GIT_BRANCH_JOB_PROPERTY)
+    queueItem = job.invoke()
+    queueItem.block_until_complete()
     wait_for_job_setup(jenkins, job_name)
     compare(job.get_scm_type(), "git")
 
 
-### Disabling for now, running into permissions errors
-# def test_get_scm_type_pipeline_scm_multibranch_BranchSource(
-#    jenkins,
-# ):
-#    job_name = "git_%s" % random_string()
-#    job = jenkins.create_multibranch_pipeline_job(
-#        job_name, MULTIBRANCH_GIT_SCM_JOB
-#    )
-#    wait_for_job_setup(jenkins, job_name)
-#    job.invoke(block=True, delay=20)
-#    compare(job[0].get_scm_type(), "git")
-#
-#
-# def test_get_scm_type_pipeline_github_multibranch_BranchSource(
-#    jenkins,
-# ):
-#    job_name = "git_%s" % random_string()
-#    job = jenkins.create_multibranch_pipeline_job(
-#        job_name, MULTIBRANCH_GITHUB_SCM_JOB
-#    )
-#    wait_for_job_setup(jenkins, job_name)
-#    job.invoke(block=True, delay=20)
-#    compare(job.get_scm_type(), "github")
+## Disabling for now, running into permissions errors
+def test_get_scm_type_pipeline_scm_multibranch_BranchSource(
+    jenkins,
+):
+    job_name = "git_%s" % random_string()
+    job = jenkins.create_multibranch_pipeline_job(
+        job_name, MULTIBRANCH_GIT_SCM_JOB
+    )
+    queueItem = job.invoke()
+    queueItem.block_until_complete()
+    wait_for_job_setup(jenkins, job_name)
+    job.invoke(block=True, delay=20)
+    compare(job[0].get_scm_type(), "git")
+
+
+def test_get_scm_type_pipeline_github_multibranch_BranchSource(
+    jenkins,
+):
+    job_name = "git_%s" % random_string()
+    job = jenkins.create_multibranch_pipeline_job(
+        job_name, MULTIBRANCH_GITHUB_SCM_JOB
+    )
+    queueItem = job.invoke()
+    queueItem.block_until_complete()
+    wait_for_job_setup(jenkins, job_name)
+    job.invoke(block=True, delay=20)
+    compare(job.get_scm_type(), "github")
