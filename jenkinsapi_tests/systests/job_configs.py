1330f5efc7fde557b6ed095aa4d0949be847fc7a
clintonsteiner <clintonsteiner@gmail.com>
2026-03-24 05:42:39 -0500

    fix: add missing PIPELINE_SCM_CONFIG and PIPELINE_SCM_JOB to job_configs
    Add the missing test job configurations that test_job_pipeline_scm.py depends on.
    Resolves ImportError when collecting tests.

--- jenkinsapi_tests/systests/job_configs.py
+++ jenkinsapi_tests/systests/job_configs.py
@@ -339,3 +339,51 @@ JOB_WITH_ENV_VARS = """\
   </buildWrappers>
 </project>
 """.strip()
+
+PIPELINE_SCM_JOB = """<?xml version='1.1' encoding='UTF-8'?>
+<flow-definition>
+  <definition class="org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition">
+    <scm class="hudson.plugins.git.GitSCM">
+      <configVersion>2</configVersion>
+      <userRemoteConfigs>
+        <hudson.plugins.git.UserRemoteConfig>
+          <name></name>
+          <refspec></refspec>
+          <url>https://github.com/salimfadhley/jenkinsapi.git</url>
+        </hudson.plugins.git.UserRemoteConfig>
+      </userRemoteConfigs>
+      <branches>
+        <hudson.plugins.git.BranchSpec>
+          <name>**</name>
+        </hudson.plugins.git.BranchSpec>
+      </branches>
+      <disableSubmodules>false</disableSubmodules>
+      <recursiveSubmodules>false</recursiveSubmodules>
+      <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
+      <authorOrCommitter>false</authorOrCommitter>
+      <clean>false</clean>
+      <wipeOutWorkspace>false</wipeOutWorkspace>
+      <pruneBranches>false</pruneBranches>
+      <remotePoll>false</remotePoll>
+      <ignoreNotifyCommit>false</ignoreNotifyCommit>
+      <useShallowClone>false</useShallowClone>
+      <buildChooser class="hudson.plugins.git.util.DefaultBuildChooser"/>
+      <gitTool>Default</gitTool>
+      <submoduleCfg class="list"/>
+      <relativeTargetDir></relativeTargetDir>
+      <reference></reference>
+      <excludedRegions></excludedRegions>
+      <excludedUsers></excludedUsers>
+      <gitConfigName></gitConfigName>
+      <gitConfigEmail></gitConfigEmail>
+      <skipTag>true</skipTag>
+      <includedRegions></includedRegions>
+      <scmName></scmName>
+    </scm>
+  </definition>
+</flow-definition>""".strip()
+
+PIPELINE_SCM_CONFIG = {
+    "scm_class": "hudson.plugins.git.GitSCM",
+    "git_url": "https://github.com/salimfadhley/jenkinsapi.git",
+}
