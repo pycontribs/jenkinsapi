dbd38fd54a929fcb0a23b05b1d4aefcb7983e643
clintonsteiner <clintonsteiner@gmail.com>
2026-03-30 22:13:19 -0500

    test: inspect API tokens via groovy

--- jenkinsapi_tests/systests/test_api_token_revocation.py
+++ jenkinsapi_tests/systests/test_api_token_revocation.py
@@ -3,12 +3,32 @@ System tests for API token revocation.
 """
 
 import logging
+import json
 import pytest
 from jenkinsapi.utils.crumb_requester import CrumbRequester
 
 log = logging.getLogger(__name__)
 
 pytestmark = pytest.mark.docker
+
+
+def _get_token_uuids(jenkins):
+    script = """
+import groovy.json.JsonOutput
+import hudson.model.User
+import jenkins.security.ApiTokenProperty
+
+def user = User.current()
+def property = user.getProperty(ApiTokenProperty)
+def token_list = property.tokenStore.tokenList.collect { token ->
+    [uuid: token.uuid]
+}
+println(JsonOutput.toJson(token_list))
+""".strip()
+    return [
+        token["uuid"]
+        for token in json.loads(jenkins.run_groovy_script(script))
+    ]
 
 
 def test_revoke_api_token(jenkins_admin_admin):
@@ -20,20 +40,12 @@ def test_revoke_api_token(jenkins_admin_admin):
     jenkins_admin_admin.poll()
 
     jenkins_admin_admin.generate_new_api_token("token-to-revoke")
+    token_uuids = _get_token_uuids(jenkins_admin_admin)
+    assert token_uuids
 
-    url = (
-        "%s/me/descriptorByName/jenkins.security.ApiTokenProperty/api/json"
-        % jenkins_admin_admin.baseurl
-    )
-    resp = jenkins_admin_admin.requester.get_url(url)
-    token_list = resp.json().get("tokenList", [])
-    assert len(token_list) >= 1
-
-    token_uuid = token_list[-1]["uuid"]
+    token_uuid = token_uuids[-1]
     jenkins_admin_admin.revoke_api_token(token_uuid)
-
-    resp = jenkins_admin_admin.requester.get_url(url)
-    remaining = [t["uuid"] for t in resp.json().get("tokenList", [])]
+    remaining = _get_token_uuids(jenkins_admin_admin)
     assert token_uuid not in remaining
 
 
@@ -48,11 +60,6 @@ def test_revoke_all_api_tokens(jenkins_admin_admin):
     jenkins_admin_admin.generate_new_api_token("token-1")
     jenkins_admin_admin.generate_new_api_token("token-2")
 
+    assert _get_token_uuids(jenkins_admin_admin)
     jenkins_admin_admin.revoke_all_api_tokens()
-
-    url = (
-        "%s/me/descriptorByName/jenkins.security.ApiTokenProperty/api/json"
-        % jenkins_admin_admin.baseurl
-    )
-    resp = jenkins_admin_admin.requester.get_url(url)
-    assert resp.json().get("tokenList", []) == []
+    assert _get_token_uuids(jenkins_admin_admin) == []
